1ac96186ca60dffcfb2d414483f4e13426ca5d4a
alexandreFialho <alexandrefialhobr@gmail.com>
2021-03-05 13:33:39 -0300

    added endpoint for create user

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -2,6 +2,14 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+@router.post("/api/users",
+            summary="Return data of the created user",
+            responses={
+                201: {"description": "User created successfully"},
+            },
+            tags=["Users"])
+async def create_user():
+    return {"message": "User created successfully"}
 
 @router.get("/api/users",
             summary="Return data from all users",
@@ -19,7 +27,7 @@ async def get_data_from_all_users():
                 200: {"description": "Data from user"},
             },
             tags=["Users"])
-async def get_data_from_user(user_id: int):
+async def get_data_from(user_id: int):
     return {"message": f"data from user: {user_id}"}
 
 
@@ -29,7 +37,7 @@ async def get_data_from_user(user_id: int):
                 202: {"description": f"User updated"},
             },
             tags=["Users"])
-async def get_data_from_user(user_id: int):
+async def update_user_from(user_id: int):
     return {"message": f"User Updated: {user_id}"}
 
 
@@ -39,5 +47,5 @@ async def get_data_from_user(user_id: int):
                    202: {"description": f"User deleted"},
                },
                tags=["Users"])
-async def get_data_from_user(user_id: int):
+async def delete_user_from(user_id: int):
     return {"message": f"User Deleted: {user_id}"}
