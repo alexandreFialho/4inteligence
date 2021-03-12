3daab295bddceac2802ca0465544d1325955e168
alexandreFialho <alexandrefialhobr@gmail.com>
2021-03-12 15:43:04 -0300

    added jwt in tests

--- app/tests/test_users_b.py
+++ app/tests/test_users_b.py
@@ -1,10 +1,28 @@
+import json
+
 from starlette.testclient import TestClient
+
+
+def test_create_auth(client: TestClient):
+    response = client.post(
+        "api/auth",
+        json={"username": "test", "password": "123teste"},
+    )
+
+    assert response.status_code == 201
+    content = response.json()
+    assert content["username"] == "test"
 
 
 def test_create(client: TestClient):
     response = client.post(
+        "token",
+        data={"username": "test", "password": "123teste"},
+    )
+    token = json.loads(response.content).get("access_token")
+    response = client.post(
         "api/users",
-        headers={"Authorization": "bearer "},
+        headers={"Authorization": f"bearer {token}"},
         json={
             "name": "Alexandre",
             "document": "123.456.789-10",
@@ -23,7 +41,7 @@ def test_create(client: TestClient):
 def test_create_bad_token(client: TestClient):
     response = client.post(
         "api/users",
-        headers={"Authorization": "bearer bad_token"},
+        headers={"Authorization": "bearer jdsailfiuhksaf"},
         json={
             "name": "Alexandre",
             "document": "123.456.789-10",
@@ -36,8 +54,13 @@ def test_create_bad_token(client: TestClient):
 
 def test_create_existing(client: TestClient):
     response = client.post(
+        "token",
+        data={"username": "test", "password": "123teste"},
+    )
+    token = json.loads(response.content).get("access_token")
+    response = client.post(
         "api/users",
-        headers={"Authorization": "bearer "},
+        headers={"Authorization": f"bearer {token}"},
         json={
             "name": "Alexandre",
             "document": "123.456.789-10",
@@ -49,7 +72,12 @@ def test_create_existing(client: TestClient):
 
 
 def test_read(client: TestClient):
-    response = client.get("api/users/1", headers={"Authorization": "bearer "})
+    response = client.post(
+        "token",
+        data={"username": "test", "password": "123teste"},
+    )
+    token = json.loads(response.content).get("access_token")
+    response = client.get("api/users/1", headers={"Authorization": f"bearer {token}"})
     assert response.status_code == 200
     assert response.json() == {
         "id": 1,
@@ -60,19 +88,31 @@ def test_read(client: TestClient):
 
 
 def test_read_bad_token(client: TestClient):
-    response = client.get("api/users/1", headers={"Authorization": "bearer "})
+    response = client.get(
+        "api/users/1", headers={"Authorization": "bearer dhasudlajkn"}
+    )
     assert response.status_code == 401
     assert response.json() == {"detail": "Could not validate credentials"}
 
 
 def test_read_inexistent(client: TestClient):
-    response = client.get("api/users/2", headers={"Authorization": "bearer "})
+    response = client.post(
+        "token",
+        data={"username": "test", "password": "123teste"},
+    )
+    token = json.loads(response.content).get("access_token")
+    response = client.get("api/users/2", headers={"Authorization": f"bearer {token}"})
     assert response.status_code == 404
     assert response.json() == {"detail": "User not found"}
 
 
 def test_read_all(client: TestClient):
-    response = client.get("api/users/", headers={"Authorization": "bearer "})
+    response = client.post(
+        "token",
+        data={"username": "test", "password": "123teste"},
+    )
+    token = json.loads(response.content).get("access_token")
+    response = client.get("api/users/", headers={"Authorization": f"bearer {token}"})
     assert response.status_code == 200
     assert response.json() == [
         {
@@ -85,15 +125,22 @@ def test_read_all(client: TestClient):
 
 
 def test_read_all_bad_token(client: TestClient):
-    response = client.get("/api/users/", headers={"Authorization": "bearer "})
+    response = client.get(
+        "/api/users/", headers={"Authorization": "bearer jdlasjhfgsfkah"}
+    )
     assert response.status_code == 401
     assert response.json() == {"detail": "Could not validate credentials"}
 
 
 def test_put(client: TestClient):
+    response = client.post(
+        "token",
+        data={"username": "test", "password": "123teste"},
+    )
+    token = json.loads(response.content).get("access_token")
     response = client.put(
         "api/users/1",
-        headers={"Authorization": "bearer "},
+        headers={"Authorization": f"bearer {token}"},
         json={
             "name": "Alexandre",
             "document": "321.456.789-10",
@@ -112,7 +159,7 @@ def test_put(client: TestClient):
 def test_put_bad_token(client: TestClient):
     response = client.put(
         "api/users/1",
-        headers={"Authorization": "bearer "},
+        headers={"Authorization": "bearer dasjhdkjfd"},
         json={
             "name": "Alexandre",
             "document": "321.456.789-10",
@@ -124,9 +171,14 @@ def test_put_bad_token(client: TestClient):
 
 
 def test_put_inexistent(client: TestClient):
+    response = client.post(
+        "token",
+        data={"username": "test", "password": "123teste"},
+    )
+    token = json.loads(response.content).get("access_token")
     response = client.put(
         "api/users/2",
-        headers={"Authorization": "bearer "},
+        headers={"Authorization": f"bearer {token}"},
         json={
             "name": "Alexandre",
             "document": "321.456.789-10",
@@ -138,18 +190,33 @@ def test_put_inexistent(client: TestClient):
 
 
 def test_delete(client: TestClient):
-    response = client.delete("api/users/1", headers={"Authorization": "bearer "})
-    assert response.status_code == 200
-    assert response.json() == {"detail": "User deleted successfully"}
+    response = client.post(
+        "token",
+        data={"username": "test", "password": "123teste"},
+    )
+    token = json.loads(response.content).get("access_token")
+    response = client.delete(
+        "api/users/1", headers={"Authorization": f"bearer {token}"}
+    )
+    assert response.status_code == 204
 
 
 def test_delete_bad_token(client: TestClient):
-    response = client.delete("api/users/1", headers={"Authorization": "bearer "})
+    response = client.delete(
+        "api/users/1", headers={"Authorization": "bearer hfhajlfdjslhf"}
+    )
     assert response.status_code == 401
     assert response.json() == {"detail": "Could not validate credentials"}
 
 
 def test_delete_inexistent(client: TestClient):
-    response = client.delete("api/users/32", headers={"Authorization": "bearer "})
+    response = client.post(
+        "token",
+        data={"username": "test", "password": "123teste"},
+    )
+    token = json.loads(response.content).get("access_token")
+    response = client.delete(
+        "api/users/32", headers={"Authorization": f"bearer {token}"}
+    )
     assert response.status_code == 404
     assert response.json() == {"detail": "User not found"}
